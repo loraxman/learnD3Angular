1ddc4c3bb0d799cb6da6a22be8f5840769d3c5fa
Ubuntu <ubuntu@ip-10-195-198-237.ec2.internal>
2013-10-18 17:18:08 +0000

    adding sqlalchemny

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
 from app import views
 
